535836e8b9c2a612cc072587223110a5a2b37f89
PavitarSharma <pavitarsharma144@gmail.com>
2023-04-15 10:48:48 +0530

    connect to mongodb database

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import '../styles/Home.module.scss'
+import '@/styles/Home.module.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  
   return (
     <>
       <Head>
@@ -14,7 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="redc">Hello</h1>
+      <h1 className="redc">Hello, World</h1>
     </>
   )
 }
